a559148048d36d6c2532940521af23fac7e5d06d
Daniel Freitas Ferreira <danielfferreirabr@gmail.com>
2022-01-14 11:10:32 -0300

    ajustando responsividade card empty space

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,21 @@
 const URL = "http://localhost:3000/documentos";
 const elemCont = document.querySelector("container");
 
-fetch(URL).then((res) => {
+// fetch(URL).then((res) => {
+//   return res.json();
+// })
+// .then((data) => {
+//   data.map(documents => {
+//     const elemDiv = document.createElement("div");
+//     elemDiv.innerHTML = documents.certidao_de_nascimento.nome_do_documento;
+//     return elemCont.innerHTML = elemDiv;
+//   });
+// });
+
+fetch(URL).then(res => {
   return res.json();
-})
-.then((data) => {
-  data.map(documents => {
-    const elemDiv = document.createElement("div");
-    elemDiv.innerHTML = documents.certidao_de_nascimento.nome_do_documento;
-    return elemCont.innerHTML = elemDiv;
-  });
+}).then(data => {
+  document.querySelector("#nome-documento").innerHTML = data[0].nome_documento;
+  console.log(data);
 });
 
