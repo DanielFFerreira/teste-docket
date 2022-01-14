a4004151f7b455aa73287dd6c739b81322c6e715
Daniel Freitas Ferreira <danielfferreirabr@gmail.com>
2022-01-14 00:25:55 -0300

    estilos required dos campos e e buscando dados na api

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+const URL = "http://localhost:3000/documentos";
+const elemCont = document.querySelector("container");
+
+fetch(URL).then((res) => {
+  return res.json();
+})
+.then((data) => {
+  data.map(documents => {
+    const elemDiv = document.createElement("div");
+    elemDiv.innerHTML = documents.certidao_de_nascimento.nome_do_documento;
+    return elemCont.innerHTML = elemDiv;
+  });
+});
+
